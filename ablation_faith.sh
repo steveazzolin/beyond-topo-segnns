1ac35d134ab6b3bfca10a521787e3c328690577b
Azzolin Steve (ACC0120483) <steve.azzolin@unitn.it@clicgpu2.cimec.unitn.it>
2024-04-16 18:15:08 +0200

    created ablation/ folder for ablation results

--- ablation_faith.sh
+++ ablation_faith.sh
@@ -3,7 +3,7 @@ set -e
 
 for DATASET in GOODCMNIST/color GOODMotif2/basis GOODMotif/size; do #GOODMotif/basis GOODMotif/size GOODMotif2/basis GOODSST2/length GOODTwitter/length GOODHIV/scaffold GOODCMNIST/color LBAPcore/assay
        for MODEL in LECI; do # CIGA LECI GSAT
-            for BUDGET in 100 500 800 1500 2500; do
+            for BUDGET in 4 8 12 16 20; do #100 500 800 1500 2500
                     goodtg --config_path final_configs/${DATASET}/covariate/${MODEL}.yaml \
                             --seeds "1/2/3/4/5" \
                             --task eval_metric \
@@ -13,11 +13,13 @@ for DATASET in GOODCMNIST/color GOODMotif2/basis GOODMotif/size; do #GOODMotif/b
                             --gpu_idx 2 \
                             --mask  \
                             --debias \
-                            --numsamples_budget ${BUDGET} \
+                            --expval_budget ${BUDGET} \
                             --samplingtype deconfounded \
                             --nec_number_samples prop_G_dataset \
                             --save_metrics \
-                            --log_id suff++_old_ablation_numsamples_budget_${BUDGET}
+                            --log_id suff++_old_ablation_expval_budget_${BUDGET}
+                        #     --log_id suff++_old_ablation_numsamples_budget_${BUDGET}                            
+                        #     --numsamples_budget ${BUDGET} \
                     echo "DONE ${MODEL} ${DATASET}"
             done
        done
