68f3cc8806e8e95cf46463bce0d32dad865c2df4
Azzolin Steve (ACC0120483) <steve.azzolin@unitn.it@clicgpu2.cimec.unitn.it>
2024-04-16 11:09:06 +0200

    ablation num_samples faith

---
+++ ablation_faith.sh
@@ -0,0 +1,26 @@
+set -e
+
+
+for DATASET in GOODCMNIST/color GOODMotif2/basis GOODMotif/size; do #GOODMotif/basis GOODMotif/size GOODMotif2/basis GOODSST2/length GOODTwitter/length GOODHIV/scaffold GOODCMNIST/color LBAPcore/assay
+       for MODEL in LECI; do # CIGA LECI GSAT
+            for BUDGET in 100 500 800 1500 2500; do
+                    goodtg --config_path final_configs/${DATASET}/covariate/${MODEL}.yaml \
+                            --seeds "1/2/3/4/5" \
+                            --task eval_metric \
+                            --metrics "suff++/nec" \
+                            --average_edge_attn mean \
+                            --mitigation_sampling feat \
+                            --gpu_idx 2 \
+                            --mask  \
+                            --debias \
+                            --numsamples_budget ${BUDGET} \
+                            --samplingtype deconfounded \
+                            --nec_number_samples prop_G_dataset \
+                            --save_metrics \
+                            --log_id suff++_old_ablation_numsamples_budget_${BUDGET}
+                    echo "DONE ${MODEL} ${DATASET}"
+            done
+       done
+done
+
+echo "DONE all :)"
