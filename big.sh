d92f28437ca8c1d73604eea6d80c6ed0deb7771f
Azzolin Steve (ACC0120483) <steve.azzolin@unitn.it@clicgpu2.cimec.unitn.it>
2024-04-17 17:31:28 +0200

    big for all mitigations

--- big.sh
+++ big.sh
@@ -1,22 +1,30 @@
 set -e
 
 
-for DATASET in GOODSST2/length GOODTwitter/length GOODHIV/scaffold LBAPcore/assay GOODCMNIST/color; do #GOODMotif/basis GOODMotif/size GOODMotif2/basis GOODSST2/length GOODTwitter/length GOODHIV/scaffold GOODCMNIST/color LBAPcore/assay
-       for MODEL in LECI CIGA GSAT; do # CIGA LECI GSAT
-              goodtg --config_path final_configs/${DATASET}/covariate/${MODEL}.yaml \
-                     --seeds "1/2/3/4/5" \
-                     --mitigation_sampling feat \
-                     --task eval_metric \
-                     --metrics "suff++/nec" \
-                     --average_edge_attn mean \
-                     --gpu_idx 2 \
-                     --mask  \
-                     --debias \
-                     --samplingtype deconfounded \
-                     --nec_number_samples prop_G_dataset \
-                     --save_metrics \
-                     --log_id suff++_old #suff_simple_old
-              echo "DONE ${MODEL} ${DATASET}"
+for DATASET in GOODMotif/basis GOODMotif2/basis GOODSST2/length GOODTwitter/length GOODMotif/size; do #GOODMotif/basis GOODMotif/size GOODMotif2/basis GOODSST2/length GOODTwitter/length GOODHIV/scaffold GOODCMNIST/color LBAPcore/assay
+       for MODEL in LECI; do # CIGA LECI GSAT
+              for EXPL_SCORES in topk hard anneal; do
+                     goodtg --config_path final_configs/${DATASET}/covariate/${MODEL}.yaml \
+                            --seeds "1/2/3/4/5" \
+                            --task eval_metric \
+                            --metrics "suff++/nec" \
+                            --average_edge_attn mean \
+                            --mitigation_sampling feat \
+                            --mitigation_readout weighted \
+                            --mitigation_virtual weighted \
+                            --mitigation_expl_scores ${EXPL_SCORES} \
+                            --mitigation_expl_scores_topk 0.8 \
+                            --gpu_idx 2 \
+                            --mask  \
+                            --debias \
+                            --samplingtype deconfounded \
+                            --nec_number_samples prop_G_dataset \
+                            --save_metrics \
+                            --log_id suff++_old_allmitig_${EXPL_SCORES} #suff_simple_old
+                            # --mitigation_readout weighted \
+                            # --model_name LECIGIN \
+                     echo "DONE ${MODEL} ${DATASET} ${EXPL_SCORES}"
+              done
        done
 done
 
