6bfd3634388f9d3f8fb97f03658246e5d9dc199d
Azzolin Steve (ACC0120483) <steve.azzolin@unitn.it@clicgpu2.cimec.unitn.it>
2025-03-26 10:25:08 +0100

    Rebuttal: faithf MUTAG

--- scripts/faithfulness_mutag.sh
+++ scripts/faithfulness_mutag.sh
@@ -4,7 +4,7 @@ echo "I'm computing faithfulness :)"
 echo "The PID of this script is: $$"
 
 SPLITS="id_val"
-SEEDS="1"
+SEEDS="1/2/3/4/5/6/7/8/9/10"
 RATIOS="0.3/0.6/0.9"
 NUMSAMPLES_BUDGET=9999999
 
@@ -50,158 +50,197 @@ for DATASET in MUTAG/basis/no_shift; do
 
 
 
-        #     goodtg --config_path final_configs/${DATASET}/GiSST.yaml \
-        #             --seeds ${SEEDS} \
-        #             --task eval_metric \
-        #             --metrics "suff_simple/nec" \
-        #             --splits ${SPLITS} \
-        #             --ratios ${RATIOS} \
-        #             --save_metrics \
-        #             --nec_alpha_1 ${NECALPHA} \
-        #             --log_id allclasses \
-        #             --numsamples_budget ${NUMSAMPLES_BUDGET} \
-        #             --average_edge_attn mean \
-        #             --use_norm bn \
-        #             --gpu_idx 1 \
-        #             --samplingtype deconfounded \
-        #             --nec_number_samples prop_G_dataset
-        #     echo "DONE GiSST ${DATASET} NEC_ALPHA ${NECALPHA}"
+            goodtg --config_path final_configs/${DATASET}/GiSST.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --save_metrics \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --use_norm none \
+                    --gpu_idx 2 \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset
+            echo "DONE GiSST ${DATASET} NEC_ALPHA ${NECALPHA}"
 
-        #     goodtg --config_path final_configs/${DATASET}/GiSST.yaml \
-        #             --seeds ${SEEDS} \
-        #             --task eval_metric \
-        #             --metrics "suff_simple/nec" \
-        #             --splits ${SPLITS} \
-        #             --ratios ${RATIOS} \
-        #             --save_metrics \
-        #             --random_expl \
-        #             --nec_alpha_1 ${NECALPHA} \
-        #             --log_id allclasses \
-        #             --numsamples_budget ${NUMSAMPLES_BUDGET} \
-        #             --average_edge_attn mean \
-        #             --use_norm bn \
-        #             --gpu_idx 1 \
-        #             --samplingtype deconfounded \
-        #             --nec_number_samples prop_G_dataset
-        #     echo "DONE GiSST ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
+            goodtg --config_path final_configs/${DATASET}/GiSST.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --save_metrics \
+                    --random_expl \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --use_norm none \
+                    --gpu_idx 2 \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset
+            echo "DONE GiSST ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
+
+
+
+            goodtg --config_path final_configs/${DATASET}/GiSST.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --save_metrics \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --global_side_channel simple_concept2temperature \
+                    --use_norm none \
+                    --gpu_idx 2 \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset
+            echo "DONE GiSST simple_concept2temperature ${DATASET} NEC_ALPHA ${NECALPHA}"
+
+            goodtg --config_path final_configs/${DATASET}/GiSST.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --save_metrics \
+                    --random_expl \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --global_side_channel simple_concept2temperature \
+                    --use_norm none \
+                    --gpu_idx 2 \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset
+            echo "DONE GiSST simple_concept2temperature ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
 
 
 
                      
                      
-            # goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
-            #         --seeds ${SEEDS} \
-            #         --task eval_metric \
-            #         --metrics "suff_simple/nec" \
-            #         --splits ${SPLITS} \
-            #         --ratios ${RATIOS} \
-            #         --samplingtype deconfounded \
-            #         --nec_number_samples prop_G_dataset \
-            #         --save_metrics \
-            #         --nec_alpha_1 ${NECALPHA} \
-            #         --log_id allclasses_recompute_motif \
-            #         --numsamples_budget ${NUMSAMPLES_BUDGET} \
-            #         --average_edge_attn mean \
-            #         --gpu_idx 1 \
-            #         --global_side_channel simple_concept2temperature \
-            #         --use_norm bn
-            # echo "DONE SMGNN simple_concept2temperature ${DATASET} NEC_ALPHA ${NECALPHA}"
+            goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset \
+                    --save_metrics \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --gpu_idx 2 \
+                    --global_side_channel simple_concept2temperature \
+                    --use_norm none
+            echo "DONE SMGNN simple_concept2temperature ${DATASET} NEC_ALPHA ${NECALPHA}"
 
-            # goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
-            #         --seeds ${SEEDS} \
-            #         --task eval_metric \
-            #         --metrics "suff_simple/nec" \
-            #         --splits ${SPLITS} \
-            #         --ratios ${RATIOS} \
-            #         --samplingtype deconfounded \
-            #         --nec_number_samples prop_G_dataset \
-            #         --save_metrics \
-            #         --random_expl \
-            #         --nec_alpha_1 ${NECALPHA} \
-            #         --log_id allclasses_recompute_motif \
-            #         --numsamples_budget ${NUMSAMPLES_BUDGET} \
-            #         --average_edge_attn mean \
-            #         --gpu_idx 1 \
-            #         --global_side_channel simple_concept2temperature \
-            #         --use_norm bn
-            # echo "DONE SMGNN simple_concept2temperature ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
-
-
-
-
-            # goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
-            #         --seeds ${SEEDS} \
-            #         --task eval_metric \
-            #         --metrics "suff_simple/nec" \
-            #         --splits ${SPLITS} \
-            #         --ratios ${RATIOS} \
-            #         --samplingtype deconfounded \
-            #         --nec_number_samples prop_G_dataset \
-            #         --save_metrics \
-            #         --nec_alpha_1 ${NECALPHA} \
-            #         --log_id allclasses_recompute_motif \
-            #         --numsamples_budget ${NUMSAMPLES_BUDGET} \
-            #         --average_edge_attn mean \
-            #         --gpu_idx 1 \
-            #         --use_norm bn
-            # echo "DONE SMGNN PLAIN ${DATASET} NEC_ALPHA ${NECALPHA}"
-
-            # goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
-            #         --seeds ${SEEDS} \
-            #         --task eval_metric \
-            #         --metrics "suff_simple/nec" \
-            #         --splits ${SPLITS} \
-            #         --ratios ${RATIOS} \
-            #         --samplingtype deconfounded \
-            #         --nec_number_samples prop_G_dataset \
-            #         --save_metrics \
-            #         --random_expl \
-            #         --nec_alpha_1 ${NECALPHA} \
-            #         --log_id allclasses_recompute_motif \
-            #         --numsamples_budget ${NUMSAMPLES_BUDGET} \
-            #         --average_edge_attn mean \
-            #         --gpu_idx 1 \
-            #         --use_norm bn
-            # echo "DONE SMGNN PLAIN ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
+            goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset \
+                    --save_metrics \
+                    --random_expl \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --gpu_idx 2 \
+                    --global_side_channel simple_concept2temperature \
+                    --use_norm none
+            echo "DONE SMGNN simple_concept2temperature ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
 
 
 
-            # goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
-            #         --seeds ${SEEDS} \
-            #         --task eval_metric \
-            #         --metrics "suff_simple/nec" \
-            #         --splits ${SPLITS} \
-            #         --ratios ${RATIOS} \
-            #         --save_metrics \
-            #         --nec_alpha_1 ${NECALPHA} \
-            #         --log_id allclasses_recompute_motif \
-            #         --numsamples_budget ${NUMSAMPLES_BUDGET} \
-            #         --average_edge_attn mean \
-            #         --use_norm bn \
-            #         --global_side_channel simple_concept2temperature \
-            #         --gpu_idx 1 \
-            #         --samplingtype deconfounded \
-            #         --nec_number_samples prop_G_dataset
-            # echo "DONE GL-GSAT temp ${DATASET} NEC_ALPHA ${NECALPHA}"
 
-            # goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
-            #          --seeds ${SEEDS} \
-            #          --task eval_metric \
-            #          --metrics "suff_simple/nec" \
-            #          --splits ${SPLITS} \
-            #          --ratios ${RATIOS} \
-            #          --save_metrics \
-            #          --random_expl \
-            #          --nec_alpha_1 ${NECALPHA} \
-            #          --log_id allclasses_recompute_motif \
-            #          --numsamples_budget ${NUMSAMPLES_BUDGET} \
-            #          --average_edge_attn mean \
-            #          --use_norm bn \
-            #          --global_side_channel simple_concept2temperature \
-            #          --gpu_idx 1 \
-            #          --samplingtype deconfounded \
-            #          --nec_number_samples prop_G_dataset
-            # echo "DONE GL-GSAT temp ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
+            goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset \
+                    --save_metrics \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --gpu_idx 2 \
+                    --use_norm none
+            echo "DONE SMGNN PLAIN ${DATASET} NEC_ALPHA ${NECALPHA}"
+
+            goodtg --config_path final_configs/${DATASET}/SMGNN.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset \
+                    --save_metrics \
+                    --random_expl \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --gpu_idx 2 \
+                    --use_norm none
+            echo "DONE SMGNN PLAIN ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
+
+
+
+            goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
+                    --seeds ${SEEDS} \
+                    --task eval_metric \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --save_metrics \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id rebuttal \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --use_norm none \
+                    --global_side_channel simple_concept2temperature \
+                    --gpu_idx 2 \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset
+            echo "DONE GL-GSAT temp ${DATASET} NEC_ALPHA ${NECALPHA}"
+
+            goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
+                     --seeds ${SEEDS} \
+                     --task eval_metric \
+                     --metrics "suff_simple/nec" \
+                     --splits ${SPLITS} \
+                     --ratios ${RATIOS} \
+                     --save_metrics \
+                     --random_expl \
+                     --nec_alpha_1 ${NECALPHA} \
+                     --log_id rebuttal \
+                     --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                     --average_edge_attn mean \
+                     --use_norm none \
+                     --global_side_channel simple_concept2temperature \
+                     --gpu_idx 2 \
+                     --samplingtype deconfounded \
+                     --nec_number_samples prop_G_dataset
+            echo "DONE GL-GSAT temp ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
 
             
        done
