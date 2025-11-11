d76eb24527123ea0837533236a0c507ea5302b08
Steve Azzolin <steve.azzolin1@gmail.com>
2025-11-11 10:46:09 +0100

    Added scripts to print faith + cleaned script files

---
+++ scripts/print_faithfulness_motif.sh
@@ -0,0 +1,31 @@
+set -e
+
+SPLITS="id_val"
+SEEDS="1/2/3/4/5/6/7/8/9/10"
+RATIOS="0.3/0.6/0.9"
+NUMSAMPLES_BUDGET=9999999
+
+for DATASET in GOODMotif/basis/covariate; do
+       for NECALPHA in 0.01 0.05 0.1; do
+
+            goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
+                    --seeds ${SEEDS} \
+                    --task print_faith \
+                    --metrics "suff_simple/nec" \
+                    --splits ${SPLITS} \
+                    --ratios ${RATIOS} \
+                    --save_metrics \
+                    --nec_alpha_1 ${NECALPHA} \
+                    --log_id allclasses \
+                    --numsamples_budget ${NUMSAMPLES_BUDGET} \
+                    --average_edge_attn mean \
+                    --use_norm bn \
+                    --global_side_channel simple_concept2temperature \
+                    --gpu_idx 1 \
+                    --samplingtype deconfounded \
+                    --nec_number_samples prop_G_dataset
+            echo "DONE DC-GSAT ${DATASET} NEC_ALPHA ${NECALPHA}"       
+       done
+done
+
+echo "DONE all :)"
--- scripts/print_faithfulness_topofeature.sh
+++ scripts/print_faithfulness_topofeature.sh
@@ -9,7 +9,7 @@ for DATASET in TopoFeature/basis/no_shift; do
 
               goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
                      --seeds ${SEEDS} \
-                     --task eval_metric \
+                     --task print_faith \
                      --metrics "suff_simple/nec" \
                      --splits ${SPLITS} \
                      --ratios ${RATIOS} \
@@ -24,27 +24,6 @@ for DATASET in TopoFeature/basis/no_shift; do
                      --samplingtype deconfounded \
                      --nec_number_samples prop_G_dataset
               echo "DONE DC-GSAT ${DATASET} NEC_ALPHA ${NECALPHA}"
-              
-              
-              # Compute FAITH over random explanations
-              goodtg --config_path final_configs/${DATASET}/GSAT.yaml \
-                     --seeds ${SEEDS} \
-                     --task eval_metric \
-                     --metrics "suff_simple/nec" \
-                     --splits ${SPLITS} \
-                     --ratios ${RATIOS} \
-                     --save_metrics \
-                     --random_expl \
-                     --nec_alpha_1 ${NECALPHA} \
-                     --log_id allclasses \
-                     --numsamples_budget ${NUMSAMPLES_BUDGET} \
-                     --average_edge_attn mean \
-                     --use_norm none \
-                     --global_side_channel simple_concept2temperature \
-                     --gpu_idx 1 \
-                     --samplingtype deconfounded \
-                     --nec_number_samples prop_G_dataset
-              echo "DONE DC-GSAT ${DATASET} NEC_ALPHA ${NECALPHA} RANDOM"
        done
 done
 
